a39f6c69d79c98ee9511e6d87c72a58c325c9dd6
barath682 <bharathkumar.r2023cse@sece.ac.in>
2025-11-01 14:15:56 +0530

    second  commit

--- resume-builder/backend/app.js
+++ resume-builder/backend/app.js
@@ -9,9 +9,11 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", // Adjust to your frontend URL
-  credentials: true,
+  origin: "http://localhost:5174",
+  credentials: true
 }));
+
+
 app.use(express.json());
 
 app.use("/auth", require("./routes/auth"));
